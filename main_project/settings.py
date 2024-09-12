b1c7bd23557bdfbade8d2f2c14e929be99c3659e
vitorsmonteiro <vitors.monteiro1@gmail.com>
2024-09-12 16:19:05 +0200

    Add django-simple-history

--- main_project/settings.py
+++ main_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "main_app",
     "django_celery_beat",
     "fontawesomefree",
+    "simple_history",
 ]
 
 
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "simple_history.middleware.HistoryRequestMiddleware",
 ]
 
 ROOT_URLCONF = "main_project.urls"
