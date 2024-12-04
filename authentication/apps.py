b96a7bd291b2fd56f70de2f96c7a5b170df71186
vitorsmonteiro <vitors.monteiro1@gmail.com>
2024-12-04 17:17:34 +0100

    Fix formatting and fixtures

--- authentication/apps.py
+++ authentication/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class AuthenticationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authentication'
+    """App configuration."""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "authentication"
--- conftest.py
+++ conftest.py
@@ -4,6 +4,8 @@ from authentication.models import User
 
 pytestmark = pytest.mark.django_db
 
+FIRST_NAME = "foo"
+LAST_NAME = "bar"
 USER_EMAIL = "foo@bar.com"
 USER_PASSWORD = "password"  # noqa: S105
 
@@ -11,7 +13,7 @@ USER_PASSWORD = "password"  # noqa: S105
 @pytest.fixture
 def user_fixture() -> User:
     """User fixture."""
-    user = User(email=USER_EMAIL)
+    user = User(first_name=FIRST_NAME, last_name=LAST_NAME, email=USER_EMAIL)
     user.set_password(USER_PASSWORD)
     user.save()
     return user
