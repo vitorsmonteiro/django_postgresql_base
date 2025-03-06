9abc407bcb416c494a5b67533fee721ca1fc7982
vitorsmonteiro <vitors.monteiro1@gmail.com>
2025-03-06 19:25:15 +0100

    Add sorting

--- todo/urls.py
+++ todo/urls.py
@@ -5,7 +5,8 @@ from todo import views
 app_name = "todo"
 
 urlpatterns = [
-    path("", views.TaskList.as_view(), name="home"),
+    path("", views.home, name="home"),
+    path("task_list", views.TaskList.as_view(), name="task_list"),
     path("task_create", views.TaskCreate.as_view(), name="task_create"),
     path("task_update/<int:pk>", views.TaskUpdate.as_view(), name="task_update"),
     path("task_delete/<int:pk>", views.TaskDelete.as_view(), name="task_delete"),
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,10 @@
 from typing import Self
 
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import QuerySet
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
@@ -9,11 +12,27 @@ from todo.forms import TaskForm
 from todo.models import Task
 
 
+@login_required
+def home(request: HttpRequest) -> HttpResponse:
+    """Todo home view.
+
+    Args:
+        request (HttpRequest): Http request object.
+
+    Returns:
+        HttpResponse: Http response object
+    """
+    tasks = Task.objects.filter(created_by=request.user)
+    tasks = tasks.order_by("created_at")
+    context = {"tasks": tasks}
+    return render(request=request, template_name="todo/task_list.html", context=context)
+
+
 class TaskList(LoginRequiredMixin, ListView):
     """Task generic list view."""
 
     model = Task
-    template_name = "todo/task_list.html"
+    template_name = "todo/components/task_table.html"
     context_object_name = "tasks"
 
     def get_queryset(self: Self) -> QuerySet:
@@ -23,8 +42,12 @@ class TaskList(LoginRequiredMixin, ListView):
             QuerySet: Task queryset.
         """
         query_set = Task.objects.filter(created_by=self.request.user)
-        query_set = query_set.order_by("created_at")
-        return super().get_queryset()
+        if sort := self.request.GET.get("sort"):
+            query_set = query_set.order_by(sort)
+        for item in self.request.GET.items():
+            if item[1] and item[0] != "sort":
+                query_set = query_set.filter(item)
+        return query_set
 
 
 class TaskCreate(LoginRequiredMixin, CreateView):
@@ -34,6 +57,20 @@ class TaskCreate(LoginRequiredMixin, CreateView):
     template_name = "todo/task_create.html"
     form_class = TaskForm
     success_url = reverse_lazy("todo:home")
+
+    def form_valid(self: Self, form: TaskForm) -> HttpResponse:
+        """Override the form valid metod to add created_by.
+
+        Args:
+            form (TaskForm): Task form object
+
+        Returns:
+            HttpResponse: Http response object
+        """
+        task: Task = form.save(commit=False)
+        task.created_by = self.request.user
+        task.save()
+        return super().form_valid(form)
 
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
